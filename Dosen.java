06d0cc980c6d8a8f5293ddb912d20982189c5895
ahitsaadr <127409072+ahitsaadr@users.noreply.github.com>
2023-06-18 19:13:59 +0700

    Dosen.java

--- Dosen.java
+++ Dosen.java
@@ -20,3 +20,10 @@ class Dosen {
         System.out.println("Teaching " + Matkul);
     }
 }
+
+//Di kelas Dosen ini, memiliki dua atribut privat, yaitu Matkul (mata kuliah) dan Id_Matkul (ID mata kuliah). 
+Pada konstruktor, kita menginisialisasi kedua atribut tersebut dengan nilai yang diberikan saat objek dibuat. 
+Metode getName() digunakan untuk mengambil nilai atribut Matkul, sedangkan metode getCapacity() digunakan untuk 
+mengambil nilai atribut Id_Matkul. Metode teach() mencetak pesan yang menunjukkan bahwa dosen sedang mengajar mata 
+kuliah yang disimpan dalam atribut Matkul.//
+
